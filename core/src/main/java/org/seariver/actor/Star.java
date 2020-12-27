040c0f6347998295f60580318723df3028870581
Mario Rezende <mariorez@gmail.com>
2020-12-27 15:48:09 -0300

    add stars to game

---
+++ core/src/main/java/org/seariver/actor/Star.java
@@ -0,0 +1,32 @@
+package org.seariver.actor;
+
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import org.seariver.BaseActor;
+
+public class Star extends BaseActor {
+
+    public Star(float x, float y, Stage s) {
+
+        super(x, y, s);
+        loadTexture("star.png");
+
+        Action pulse = Actions.sequence(
+                Actions.scaleTo(1.2f, 1.2f, 0.5f),
+                Actions.scaleTo(1.0f, 1.0f, 0.5f));
+        addAction(Actions.forever(pulse));
+
+        setSpeed(100);
+        setMotionAngle(180);
+    }
+
+    public void act(float dt) {
+        super.act(dt);
+        applyPhysics(dt);
+
+        // remove after moving past left edge of screen
+        if (getX() + getWidth() < 0)
+            remove();
+    }
+}
--- core/src/main/java/org/seariver/screen/LevelScreen.java
+++ core/src/main/java/org/seariver/screen/LevelScreen.java
@@ -1,15 +1,25 @@
 package org.seariver.screen;
 
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import org.seariver.BaseActor;
+import org.seariver.BaseGame;
 import org.seariver.BaseScreen;
 import org.seariver.actor.Ground;
 import org.seariver.actor.Plane;
 import org.seariver.actor.Sky;
+import org.seariver.actor.Star;
 
 public class LevelScreen extends BaseScreen {
 
     Plane plane;
+
+    float starTimer;
+    float starSpawnInterval;
+
+    int score;
+    Label scoreLabel;
 
     public void initialize() {
 
@@ -20,9 +30,34 @@ public class LevelScreen extends BaseScreen {
 
         plane = new Plane(100, 500, mainStage);
         BaseActor.setWorldBounds(800, 600);
+
+        starTimer = 0;
+        starSpawnInterval = 4;
+        score = 0;
+
+        scoreLabel = new Label(Integer.toString(score), BaseGame.labelStyle);
+        uiTable.pad(10);
+        uiTable.add(scoreLabel);
+        uiTable.row();
+        uiTable.add().expandY();
     }
 
     public void update(float deltaTime) {
+
+        starTimer += deltaTime;
+
+        if (starTimer > starSpawnInterval) {
+            new Star(800, MathUtils.random(100, 500), mainStage);
+            starTimer = 0;
+        }
+
+        for (BaseActor star : BaseActor.getList(mainStage, "org.seariver.actor.Star")) {
+            if (plane.overlaps(star)) {
+                star.remove();
+                score++;
+                scoreLabel.setText(Integer.toString(score));
+            }
+        }
     }
 
     public boolean keyDown(int keyCode) {
